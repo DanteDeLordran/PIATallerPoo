771f1971544e9340571f955057ad2394856cd158
Lordran <dantelopez305@outlook.com>
2022-11-01 20:39:38 -0600

    Created models for db tables

---
+++ src/main/java/com/tpoo/pia/Models/Operador.java
@@ -0,0 +1,36 @@
+package com.tpoo.pia.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "operador" )
+public class Operador {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( unique = true, nullable = false ,name = "id" ,columnDefinition = "tinyint")
+    Integer id_opdr;
+
+    @Column( nullable = false, name = "nombre" , columnDefinition = "char(50)" )
+    String nom_opdr;
+
+    public Operador(){}
+
+    public Operador(String nom_opd){
+        this.nom_opdr = nom_opd;
+    }
+
+    public String getNombre(){
+        return this.nom_opdr;
+    }
+
+    public void setNombre( String nom_opdr ){
+        this.nom_opdr = nom_opdr;
+    }
+
+}
